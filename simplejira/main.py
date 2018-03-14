fc43c72c350a26dde437ef04fb7f34bb162ab823
Raoul Snyman <rsnyman@redhat.com>
2018-03-14 15:11:06 -0700

    Make the ASCII art print correctly on both Py2 and Py3, and changed all strings in main.py to use ' to be consistent

--- simplejira/main.py
+++ simplejira/main.py
@@ -18,8 +18,8 @@ def _create_config_file():
     new_config = editor_ignore_comments(get_default_config())
 
     filename = prompter.prompt(
-        "Enter path for saving config", default=CONFIG_FILE)
-    print("Writing config to {}".format(filename))
+        'Enter path for saving config', default=CONFIG_FILE)
+    print('Writing config to {}'.format(filename))
     with open(filename, 'w') as f:
         f.write(new_config)
     os.chmod(filename, 0o600)
@@ -28,8 +28,8 @@ def _create_config_file():
 
 def _setup_config():
     filename = None
-    print("It looks like you have no config created.\n")
-    if prompter.yesno("Create one now?"):
+    print('It looks like you have no config created.\n')
+    if prompter.yesno('Create one now?'):
         filename = _create_config_file()
     return filename
 
@@ -46,11 +46,11 @@ def main():
         filename = _setup_config()
 
     if not filename:
-        print("No valid config file found.")
+        print('No valid config file found.')
         sys.exit(0)
 
     # print welcome msg
-    print(get_ascii_art())
+    print(get_ascii_art().decode('utf8'))
 
     sys.argv = sys.argv[:1] + unknown_args
     MainPrompt(config_file=filename).cmdloop()
